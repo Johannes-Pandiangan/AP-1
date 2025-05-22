a32ac4b2dd4927cfb8481f3bd78633d9e8461c8c
Johannes-Pandiangan <johannesindra@students.usu.ac.id>
2025-05-22 22:49:26 +0700

    Pertemuan 2: Penggunaan Operator

--- pertemuan 2/operator.cpp
+++ pertemuan 2/operator.cpp
@@ -1,17 +1,16 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     int a,b;
     system("cls");
 
-    //assigment operator
+    //assigment operator, memberikan nilai pada suatu variabel
     a=3;
     b=5;
 
     /*
-    //arithmetical operator(+, -, /, *, %)
+    //arithmetical operator(+, -, /, *, %), operator untuk operasi matematika
     int tambah =a+b;
     int kurang = a-b;
     int kali = a*b;
@@ -25,7 +24,7 @@ int main()
     cout<<"hasil sisa bagi : "<<modulo<<endl;*/
 
     /*
-    //relational operator
+    //relational operator, operator untuk membandingkan nilai dari 2 operand
     cout<<(a==b)<<endl;
     cout<<(a>b)<<endl;
     cout<<(a>=b)<<endl;
@@ -34,7 +33,7 @@ int main()
     cout<<(a!=b)<<endl;*/
     
     /*
-    //logical operator(&&,||,!)
+    //logical operator(&&,||,!), operator untuk membandingkan 2 variabel bertipe boolean
     cout<<(true && true)<<endl;
     cout<<(true && false)<<endl;
     cout<<(false && true)<<endl;
@@ -43,12 +42,11 @@ int main()
     /*cout<<(true || true)<<endl;
     cout<<(true || false)<<endl;
     cout<<(false || true)<<endl;
-    cout<<(false || false)<<endl;*/
-
-    /*cout<<!true<<endl;
+    cout<<(false || false)<<endl;
+    cout<<!true<<endl;
     cout<<!false<<endl;*/
 
-    /*//bitwise operator (&,|,^, ~, <<, >>) --> operasi biner
+    /*//bitwise operator (&,|,^, ~, <<, >>), operator untuk operasi biner
     cout<<(5%7)<<endl;
     cout<<(5|7)<<endl;
     cout<<(5^7)<<endl;
@@ -56,7 +54,7 @@ int main()
     cout<<(7<<2)<<endl;
     cout<<(7>>2)<<endl;*/
 
-    /*//Shorthand
+    /*//Shorthand -> cara singkat dalam penulisan kode operasi
     // a+=7 --> a=a+7
     a+=7; //a=a+7
     cout<<a<<endl;
@@ -70,14 +68,16 @@ int main()
     a/=7; //a=a/7
     cout<<a<<endl;*/
 
-    /*//increment & decrement
-    //pre increment
+    //increment & decrement
+    //pre increment, penjumlahan 1 dan langsung berubah pada saat ditampilkan
     cout<<a<<endl;
     cout<<++a<<endl;
     cout<<b<<endl;
     cout<<++b<<endl;
 
-    //post increment
+    //post increment, 
+    /*penjumlahan 1 tapi tidak langsung berubah saat ditampilkan 
+    melainkan menyimpan nilai itu dan akan berubah pada saat penampilan berikutnya*/
     cout<<a<<endl;
     cout<<a++<<endl;
     cout<<a<<endl;
@@ -85,18 +85,20 @@ int main()
     cout<<b++<<endl;
     cout<<b<<endl;
 
-    //pre decrement
+    //pre decrement, pengurangan 1 dan langsung berubah pada saat ditampilkan
     cout<<a<<endl;
     cout<<--a<<endl;
     cout<<b<<endl;
     cout<<--b<<endl;
 
     //post decrerment
+     /*penjumlahan 1 tapi tidak langsung berubah saat ditampilkan 
+    melainkan menyimpan nilai itu dan akan berubah pada saat penampilan berikutnya*/
     cout<<a<<endl;
     cout<<a--<<endl;
     cout<<a<<endl;
     cout<<b<<endl;
     cout<<b--<<endl;
-    cout<<b<<endl;*/
+    cout<<b<<endl;
 
 }
