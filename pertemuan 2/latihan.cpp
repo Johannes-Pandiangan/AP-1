7af9f76c55f2d9dffe39db682358521ed2ba32f3
Johannes-Pandiangan <johannesindra@students.usu.ac.id>
2025-05-22 22:26:45 +0700

    Pertemuan 2: Penggunaan Operator

--- pertemuan 2/latihan.cpp
+++ pertemuan 2/latihan.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
     system("cls");
-    const float phi=3.14;
+    const float phi=3.14; //deklarasi konstanta
     float r,volume,luas;
     cout<<"masukkan jari-jari: ";
     cin>>r;
 
-    volume=((float)4/(float)3)*phi*r*r*r;
-    luas=4*phi*r*r;
+    volume=((float)4/(float)3)*phi*r*r*r; //rumus volume bola
+    luas=4*phi*r*r; //rumus luas bola
 
     cout<<"volume bola= "<<volume<<endl;
     cout<<"luas permukaan bola= "<<luas<<endl;
