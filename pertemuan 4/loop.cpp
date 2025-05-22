37b437d930cdf1cb07ecc0f4913754a7c0f095fa
Johannes-Pandiangan <johannesindra@students.usu.ac.id>
2025-05-22 23:24:37 +0700

    Pertemuan 4: Perulangan

--- pertemuan 4/loop.cpp
+++ pertemuan 4/loop.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     system("cls");
-    // Goto label
+    // Goto label, melakukan peloncatan ke suatu bagian tertentu dalam program ini
     // Hello world, Fasilkom-TI, Ilmu Komputer, IKLC
-    //Hello world, IKLC, Ilmu Komputer, Fasilkom-TI
+    // Hello world, IKLC, Ilmu Komputer, Fasilkom-TI
 
     /*a: cout<<"Hello world"<<endl;
         goto d;
@@ -28,7 +28,7 @@ int main()
         goto genap;
     }
 
-    // statement while
+    // statement while, statement perulangan yang akan mengulang program selama kondisi masih bernilai true
     int i=1;
     while (i<=10){
         if (i % 2 == 0) {
@@ -36,21 +36,21 @@ int main()
         } i++;
     }
 
-    //statement do-while
+    //statement do-while, sama seperti while, cuma bedanya ini akan menjalan terlebih dahulu lalu memeriksa kondisi
+    //pokoknya do-while akan menjalan minimal 1 kali program meskipun kondisi bernilai false
     int i = 1;
     do {
         cout<<i<<endl;
     } while (i<=0);
 
-    //statement for 
+    //statement for, statement perulangan sama seperti while tetapi ini memiliki inisialisasi dan increase statement
     // for (inisialisasi, kondisi, increase)
     for (int i=1; i<=10; i+=2){
         cout<<"Hello world"<<endl;
     }
 
-    //nested for
+    //nested for, for didalam for 
     // ***** 5x5
-
     for (int i = 1; i<=5; i++) {
         for (int j = 1; j<=5; j++){
             cout<<"*";
