fcf3c3ad63abc3f9d459f4e018786cf175b7847e
Johannes-Pandiangan <johannesindra@students.usu.ac.id>
2025-05-24 10:40:15 +0700

    Pertemuan 5: Array & Vector

--- pertemuan 5/vector.cpp
+++ pertemuan 5/vector.cpp
@@ -7,26 +7,27 @@ int main()
     system("cls");
     //vector declaration & initialization
 
+    //cara mendeklarasikan dan inisialisasi vector
     vector<string>nama_karyawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzakwan"};
 
-    // for(string karyawan : nama_karyawan){
+    // for(string karyawan : nama_karyawan){ //mengakses semua element vector
     //     cout<<karyawan<<endl;
     // } 
 
-    //add data to vector index terakhir
+    //add data to vector pada index terakhir
     nama_karyawan.push_back("Johannes");
 
-    // for (int i=0;i<nama_karyawan.size();i++){
+    // for (int i=0;i<nama_karyawan.size();i++){  //mengakses element vector
     //     cout<<nama_karyawan[i]<<endl;
-    // }
+    // } 
 
     //Delete data form vector index terakhir
     nama_karyawan.pop_back();
 
     //Delete data form vector index tertentu
-    nama_karyawan.erase(nama_karyawan.begin()+3);
+    nama_karyawan.erase(nama_karyawan.begin()+3); //hapus data pada index ketiga
 
-    for (int i=0;i<nama_karyawan.size();i++){
+    for (int i=0;i<nama_karyawan.size();i++){ //kembali mengakses vector
         cout<<nama_karyawan[i]<<endl;
     }
 }
