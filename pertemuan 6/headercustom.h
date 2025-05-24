19154cd07c4a7d2d73fab0caa5bae159822b3be0
Johannes-Pandiangan <johannesindra@students.usu.ac.id>
2025-05-24 11:09:12 +0700

    Pertemuan 6: Fungsi dan Header

--- pertemuan 6/headercustom.h
+++ pertemuan 6/headercustom.h
@@ -1,7 +1,9 @@
-#define u using namespace std;
+//ini adalah file headercustom.h tadi yang kita akses pada file header.cpp
+#define u using namespace std; //ini berarti mengganti using namespace std; menjadi u
 #define l <<
 #define o cout
 #define e endl
 #define r return
 #define s sqrt
-#define clear system("cls")
+#define clear system("cls") //masih banyak yang bisa diganti lagi seperti if,for,while,system ("pause") dan lainnya
+//kita juga bisa mengganti perintah yang lain sesuai dengan nama yang kita inginkan
