ba3b9839e0cec7559271e163e7c6ad93e42fcfdb
Johannes-Pandiangan <johannesindra@students.usu.ac.id>
2025-05-24 11:40:12 +0700

    Pertemuan 8 :Struct & Class

--- pertemuan 8/aksesStruct.cpp
+++ pertemuan 8/aksesStruct.cpp
@@ -2,6 +2,7 @@
 #include<string>
 using namespace std;
 
+//mendeklarasikan struct
 struct Mahasiswa {
     string nama;
     int umur;
@@ -10,17 +11,18 @@ struct Mahasiswa {
 
 int main()
 {   system("cls");
-    Mahasiswa mhs1;
+    Mahasiswa mhs1; //mengakses struct
     mhs1.nama = "Johannes";
     mhs1.umur = 18;
     mhs1.ipk = 4.00;
 
+    //cara akses struct yang biasa digunakan
     // cout<<"Akses dengan . :"<<endl;
     // cout<<"Nama: "<<mhs1.nama<<endl;
     // cout<<"Umur: "<<mhs1.umur<<endl;
     // cout<<"IPK: "<<mhs1.ipk<<endl;
 
-    Mahasiswa *ptrMhs = &mhs1;
+    Mahasiswa *ptrMhs = &mhs1; //akses struct dengan menggunakan pointer
     cout<<"Akses dengan -> :"<<endl;
     cout<<"Nama: "<<ptrMhs->nama<<endl;
     cout<<"Umur: "<<ptrMhs->umur<<endl;
